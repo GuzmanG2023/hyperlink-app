23a3c78e2190f8f17d69dc35465dc721d61b4784
eon <noe.navarro1019@gmail.com>
2022-02-09 21:00:45 -0700

    creation of Game, System, User models

---
+++ models/Game.js
@@ -0,0 +1,55 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Game extends Model {}
+
+Game.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        system_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'system',
+                key: 'id'
+            }
+        },
+
+        release_year: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+
+        publisher: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+
+        genre_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'genre',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'game'
+    }
+)
+
+module.exports = Game;
