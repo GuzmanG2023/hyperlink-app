ab2ffa84353621b704d8db18a5dcf732e03c60ad
Ian Linton <ilinton2571@gmail.com>
2022-02-12 14:27:38 -0700

    updated home routes

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,3 @@ const router = require('express');
 const homeRoutes = require('./home-routes.js');
 
 router.use('/', homeRoutes);
-
-router.get('/', (req, res) => {
-    res.render('homepage');
-});
-
-module.exports = router;
