170daa840fa99763f44a80f2a7d50bfa9e4d1690
Ian Linton <ilinton2571@gmail.com>
2022-02-12 17:49:05 -0700

    added homepage routes for getting posts

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
 const routes = require('./controllers/');
-const exphbs = require('express-handelbars');
+const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
-app.engine('handlebars', 'hbs.engine');
-app.set('view engine', 'handlebars');
-=======
 const express = require('express');
 
 const app = express();
@@ -14,8 +10,10 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.engine('handlebars', 'hbs.engine');
+app.set('view engine', 'handlebars');
+
 // point towards controller and api endpoints
 app.use(require('./controllers/'));
 
-app.listen(PORT, () => console.log(`Now listening on posrt ${PORT}`));
->>>>>>> 6cc666e38b6a50cfb542a910bfbf80113e349661
+app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
