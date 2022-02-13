46eeac7b7a934d91577fe7b4862c7c853fc8930e
Ian Linton <ilinton2571@gmail.com>
2022-02-12 17:51:38 -0700

    commented out code for server functionality

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const routes = require('./controllers/');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+// const exphbs = require('express-handlebars');
+// const hbs = exphbs.create({});
 
 const express = require('express');
 
@@ -10,10 +10,10 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.engine('handlebars', 'hbs.engine');
-app.set('view engine', 'handlebars');
+// app.engine('handlebars', 'hbs.engine');
+// app.set('view engine', 'handlebars');
 
 // point towards controller and api endpoints
-app.use(require('./controllers/'));
+// app.use(require('./controllers/'));
 
 app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
