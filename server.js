528173befbd3bb2ab281f1c50ff3cdcf168604f9
Ian Linton <ilinton2571@gmail.com>
2022-02-13 15:36:19 -0700

    resolved server launch issues

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
-const routes = require('./controllers/');
-// const exphbs = require('express-handlebars');
-// const hbs = exphbs.create({});
-
-// app.engine('handlebars', 'hbs.engine');
-// app.set('view engine', 'handlebars');
+const path = require('path');
 const express = require('express');
+const routes = require('./controllers/');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require("./config/connection");
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// app.engine('handlebars', 'hbs.engine');
-// app.set('view engine', 'handlebars');
-
 // point towards controller and api endpoints
-// app.use(require('./controllers/'));
+app.use(require('./controllers/'));
 
 app.listen(PORT, () => console.log(`Now listening on post ${PORT}`));
