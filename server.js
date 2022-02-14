44524ab1a0e603ee0a4d49caeaa0fa09d8186239
eon <noe.navarro1019@gmail.com>
2022-02-14 10:30:11 -0700

    added session calibration to the server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const routes = require('./controllers/');
+const express = require('express-session');
 // const exphbs = require('express-handlebars');
 // const hbs = exphbs.create({});
 
@@ -8,6 +9,20 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// session connectivity
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
