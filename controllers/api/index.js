4fbf4d398e96a83f0ae14a65c885f00806bf2b7c
St4d3ntG4zm4n <bwonder91@live.com>
2022-02-15 11:56:15 -0700

    saving changes on the routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
 
 // import api routes
-// const userRoutes = require('./user-routes.js');
+const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
 // TODO: add other table specific api call routes
 
-// router.use('/users', userRoutes);
+router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
+
 
 module.exports = router;
