42b21b7384a5e11a53b2000f9bbe3f3737712d36
eon <noe.navarro1019@gmail.com>
2022-02-20 03:07:04 -0700

    adding checkpoint to dev, need to finalize edit post calls

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,15 +1,27 @@
+// find buttons
+var savePostBtn = document.getElementById('save-post-btn');
+var deletePostBtn = document.getElementById('delete-post-btn');
+var postId = document.getElementById('post-id');
+var title = document.getElementById('post-title').value.trim();
+var post_data = document.getElementById('post-text').value.trim();
+
+console.log(title);
+console.log(post_data);
+console.log(postId.dataset.id);
+
 async function editFormHandler(event) {
     event.preventDefault();
 
-    const title = document.querySelector('input[name="post-title"]').value.trim();
-    const id = window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
-    ];
+    const title = document.getElementById('post-title').value.trim();
+    const post_data = document.getElementById('post-text').value.trim();
+    const id = postId.dataset.id;
 
-    const response = await fetch(`./api/posts/${id}`, {
+
+    const response = await fetch(`/api/posts/${id}`, {
         method: 'PUT',
         body: JSON.stringify({
-            title
+            title,
+            post_data
         }),
         headers: {
             'Content-Type': 'application/json'
@@ -23,4 +35,4 @@ async function editFormHandler(event) {
     }
 }
 
-document.querySelector('edit-post-form').addEventListener('submit', editFormHandler);
+savePostBtn.addEventListener('submit', editFormHandler);
